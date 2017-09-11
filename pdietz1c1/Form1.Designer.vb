9dad316a0997b242c9bb86dff770a775071677c5
Paige Dietz <si1129jl@go.minnstate.edu>
2017-09-11 17:24:05 -0500

    Test commit 3

--- pdietz1c1/Form1.Designer.vb
+++ pdietz1c1/Form1.Designer.vb
@@ -28,7 +28,7 @@ Partial Class Form1
         '
         'PictureBox1
         '
-        Me.PictureBox1.Location = New System.Drawing.Point(49, 32)
+        Me.PictureBox1.Location = New System.Drawing.Point(90, 89)
         Me.PictureBox1.Name = "PictureBox1"
         Me.PictureBox1.Size = New System.Drawing.Size(100, 50)
         Me.PictureBox1.TabIndex = 0
